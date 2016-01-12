59ceb84179f0850c4381423f29086e79b6d15440
Charles Meredith <charles3m@gmail.com>
2016-01-11 20:40:07 -0800

    [feat] finishes basic signup component

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Signup from './Signup.js';
 
 class Home extends React.Component {
   render(){
     return (
       <div>
-        <h1>HOME PAGE</h1>
+        <Signup />
       </div>
     )
   }
