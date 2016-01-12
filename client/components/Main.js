24190d1b4f905718f077da9278e69e77e04c6d4f
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-12 15:36:33 -0800

    [refactor] refactors routing for views on Main component

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,13 +1,59 @@
 import React from 'react';
+import Home from './Home.js';
+import Profile from './Profile.js';
+import PublicProfile from './PublicProfile.js';
 import Footer from './Footer.js';
 
 class Main extends React.Component {
+  constructor(){
+    super();
+    this.updateView = this.updateView.bind(this);
+    this.state = {
+      isLoggedIn: false,
+      showHome: true,
+      showProfile: false,
+      showPublicProfile: false,
+      userInfo: {
+        username: '',
+        wishlist: []
+      }
+    }
+  }
+
+  updateView(action, data){
+    let self = this;
+    switch(action){
+      case 'showHome':
+        self.setState({isLoggedIn: false});
+        self.setState({showHome: true});
+        self.setState({showProfile: false});
+        self.setState({showPublicProfile: false});
+        self.setState({userInfo: {username: '', wishlist: []}});
+        break;
+      case 'showProfile':
+        self.setState({isLoggedIn: true});
+        self.setState({showHome: false});
+        self.setState({showProfile: true});
+        self.setState({showPublicProfile: false});
+        self.setState({userInfo: data});
+        break;
+      case 'showPublicProfile':
+        self.setState({showHome: false});
+        self.setState({showProfile: false});
+        self.setState({showPublicProfile: true});
+        self.setState({userInfo: {username: '', wishlist: []}});
+        break;
+    }
+  }
+
   render(){
     return (
       <div>
         <h1>Jimini</h1>
         <div className='container'>
-          {this.props.children}
+          {this.state.showHome ? <Home updateView={this.state.updateView} /> : null}
+          {this.state.showProfile ? <Profile updateView={this.state.updateView} userInfo={this.state.userInfo} /> : null}
+          {this.state.showPublicProfile ? <PublicProfile updateView={this.state.updateView} /> : null}
         </div>
         <div>
           <Footer />
