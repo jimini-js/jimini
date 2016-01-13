98485dc739ccec09b7ec89cee9f72ee49925df8e
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-13 15:22:38 -0800

    [fix] fixes wishlist items rendering on wishlist after submit

--- client/components/ItemForm.js
+++ client/components/ItemForm.js
@@ -6,7 +6,6 @@ class ItemForm extends React.Component {
   constructor(){
     super();
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
     this.getItemnameRef = this.getItemnameRef.bind(this);
     this.getCategoryRef = this.getCategoryRef.bind(this);
     this.getMessageRef = this.getMessageRef.bind(this);
@@ -37,7 +36,7 @@ class ItemForm extends React.Component {
 
   handleSubmit(e){
     e.preventDefault();
-    this.props.updateWishlist(this.state);
+    let self = this;
     let username = this.props.userInfo.username;
     let itemname = this.itemnameRef.value;
     let category = this.categoryRef.value;
@@ -59,6 +58,7 @@ class ItemForm extends React.Component {
       success: function(data){
         console.log('post to /wishlist success');
         console.log('data from server: ', data);
+        self.props.updateWishlist(data);
       },
       error: function(err){
         console.log('error posting to wishlist: ', err);
@@ -71,26 +71,22 @@ class ItemForm extends React.Component {
     this.messageRef.value = '';
   }
 
-  handleChange(e){
-    this.setState({[e.target.name]: e.target.value});
-  }
-
   render(){
     return (
       <form ref='form'>
           <label>Item</label>
-          <input type='text' name='itemname' onChange={this.handleChange} className='form-control' placeholder='Item Name' ref={this.getItemnameRef} />
+          <input type='text' name='itemname' className='form-control' placeholder='Item Name' ref={this.getItemnameRef} />
         <div className='form-group'>
           <label>Category</label>
-          <input type='text' name='category' onChange={this.handleChange} className='form-control' placeholder='Category' ref={this.getCategoryRef} />
+          <input type='text' name='category' className='form-control' placeholder='Category' ref={this.getCategoryRef} />
         </div>
         <div className='form-group'>
           <label>Message</label>
-          <input type='text' name='message' onChange={this.handleChange} className='form-control' placeholder='Message' ref={this.getMessageRef} />
+          <input type='text' name='message' className='form-control' placeholder='Message' ref={this.getMessageRef} />
         </div>
         <div className='form-group'>
           <label>Link</label>
-          <input type='url' name='url' onChange={this.handleChange} className='form-control' placeholder='Enter URL Here' ref={this.getUrlRef} />
+          <input type='url' name='url' className='form-control' placeholder='Enter URL Here' ref={this.getUrlRef} />
         </div>
         <button type='submit' className='btn btn-primary' onClick={this.handleSubmit}>Submit</button>
       </form>
