40da09756a4bf4947098372972d7655fcbf88238
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-14 10:24:33 -0800

    [feat] adds removal button and logs id of wishlist item

--- client/components/Item.js
+++ client/components/Item.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 class Item extends React.Component {
+  constructor(){
+    super()
+    this.handleRemove = this.handleRemove.bind(this);
+  }
+
+  handleRemove(){
+    let wishId = this.props.id;
+
+    this.props.removeWish(wishId);
+  }
+
   render(){
     return (
       <div className='col-md-4'>
@@ -8,6 +19,7 @@ class Item extends React.Component {
         <p>{this.props.category}</p>
         <p>{this.props.message}</p>
         <a href={this.props.url}>{this.props.url}</a>
+        <button type='button' className='btn btn-info' onClick={this.handleRemove}>Remove Item</button>
       </div>
     )
   }
