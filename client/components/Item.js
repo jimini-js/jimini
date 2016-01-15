c037fb8263b41253524aa80cb3b46a930882fbd6
Charles Meredith <charles3m@gmail.com>
2016-01-14 19:54:10 -0800

    [style] adds React-Bootstrap elements for styling the Item component, adds an assets directory to client, and adds icon images for various items to the assets directory

--- client/components/Item.js
+++ client/components/Item.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import $ from 'jquery';
+// import { Button } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+import { Thumbnail } from 'react-bootstrap';
 
 class Item extends React.Component {
   constructor(){
@@ -30,6 +36,40 @@ class Item extends React.Component {
     }
 
 
+  render(){
+    let removeButton;
+
+    if(this.props.isLoggedIn){
+      console.log('this.props.isLoggedIn in isLoggedIn ', this.props.isLoggedIn);
+      removeButton = (<button type='button' className='btn btn-info' onClick={this.handleRemove}>Remove Item</button>);
+    }
+
+    const thumbnailInstance = (
+        <Col xs={6} md={4}>
+          <Thumbnail src="../assets/macbook_icon.png" alt="242x200">
+            {console.log('this.props in thumbnailInstance ', this.props)}
+            <h3>{this.props.itemname}</h3>
+            <p>{this.props.category}</p>
+            <p>{this.props.message}</p>
+            <a href={this.props.url}>{this.props.url}</a>
+            {removeButton}
+          </Thumbnail>
+        </Col>
+    );
+
+    return (
+      <div>
+        {thumbnailInstance}
+        {removeButton}
+      </div>
+    )
+  }
+}
+
+export default Item;
+
+
+/*
     return (
       <div className='col-md-4'>
         <h3>{this.props.itemname}</h3>
@@ -38,9 +78,32 @@ class Item extends React.Component {
         <p>{isPurchased}</p>
         <a href={this.props.url}>{this.props.url}</a>
         {changeButton}
+        {removeButton}
       </div>
     )
   }
 }
 
 export default Item;
+*/
+
+
+/*
+
+    const thumbnailInstance = (
+      <Grid>
+        <Row>
+        <Col xs={6} md={4}>
+          <Thumbnail src="client/assets/macbook_icon.png" alt="242x200">
+            <h3>{this.props.itemname}</h3>
+            <p>{this.props.category}</p>
+            <p>{this.props.message}</p>
+            <a href={this.props.url}>{this.props.url}</a>
+            {removeButton}
+          </Thumbnail>
+        </Col>
+        </Row>
+      </Grid>
+    );
+
+*/
