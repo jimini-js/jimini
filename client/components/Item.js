6cdd7cc969b9b285cd693f0b4df945d490452399
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-14 16:13:50 -0800

    [feat] passes isPurchased value to individual item component

--- client/components/Item.js
+++ client/components/Item.js
@@ -19,11 +19,14 @@ class Item extends React.Component {
       removeButton = (<button type='button' className='btn btn-info' onClick={this.handleRemove}>Remove Item</button>);
     }
 
+    let isPurchased = this.props.isPurchased.toString();
+
     return (
       <div className='col-md-4'>
         <h3>{this.props.itemname}</h3>
         <p>{this.props.category}</p>
         <p>{this.props.message}</p>
+        <p>{isPurchased}</p>
         <a href={this.props.url}>{this.props.url}</a>
         {removeButton}
       </div>
