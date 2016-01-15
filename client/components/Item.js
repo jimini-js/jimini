ba0fb45e6f9a68ce763e876086eca498b55ce5d8
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-15 15:08:43 -0800

    [pull] save progress

--- client/components/Item.js
+++ client/components/Item.js
@@ -27,32 +27,38 @@ class Item extends React.Component {
 
   render(){
     let changeButton;
+    let thumbnailInstance;
     let isPurchased = this.props.isPurchased.toString();
 
     if (this.props.isLoggedIn) {
-      changeButton = (<button type='button' className='btn btn-info' onClick={this.handleRemove}>Remove Item</button>);
-    } else {
-      changeButton = (
-        <div>
-          <input type='text' placeholder='Name' id={'name'+this.props.id} />
-          <input type='text' placeholder='Message' id={'message'+this.props.id} />
-          <button type='button' className='btn btn-info' onClick={this.handleBought}>Purchase Item</button>
-        </div>
-      )
-    }
-
-    const thumbnailInstance = (
+      thumbnailInstance = (
         <Col xs={6} md={4}>
           <Thumbnail src="../assets/macbook_icon.png" alt="242x200">
             <h3>{this.props.itemname}</h3>
             <p>{this.props.category}</p>
             <p>{this.props.message}</p>
             <a href={this.props.url}>{this.props.url}</a>
-            <div>{changeButton}</div>
+            <button type='button' className='btn btn-info' onClick={this.handleRemove}>Remove Item</button>
           </Thumbnail>
         </Col>
-    );
-
+      )
+    } else {
+      thumbnailInstance = (
+        <Col xs={6} md={4}>
+          <Thumbnail src="../assets/macbook_icon.png" alt="242x200">
+            <h3>{this.props.itemname}</h3>
+            <p>{this.props.category}</p>
+            <p>{this.props.message}</p>
+            <a href={this.props.url}>{this.props.url}</a>
+            <div>
+              <input type='text' placeholder='Name' id={'name'+this.props.id} />
+              <input type='text' placeholder='Message' id={'message'+this.props.id} />
+              <button type='button' className='btn btn-info' onClick={this.handleBought}>Purchase Item</button>
+            </div>
+          </Thumbnail>
+        </Col>
+      )
+    }
 
     return (
       <div>
