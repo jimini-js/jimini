63b895526a0c5b00e94b8896c50527a9ce42e2b3
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-15 10:13:33 -0800

    [feat] logs category for all categories

--- client/components/Categories.js
+++ client/components/Categories.js
@@ -4,10 +4,10 @@ import $ from 'jquery';
 class Categories extends React.Component {
   constructor(){
     super();
-    this.logCategory = this.logCategory.bind(this);
+    this.setCategory = this.setCategory.bind(this);
   }
 
-  logCategory(ref){
+  setCategory(ref){
     let category = ref.target.textContent ? ref.target.textContent : ref.target.alt
 
     console.log("CATEGORY:", category);
@@ -17,67 +17,67 @@ class Categories extends React.Component {
     return(
       <div className='btn-group-vertical' role='group' aria-label='...'>
         <div className="btn-group btn-group-justified" aria-label="...">
-          <div className="btn-group" role="group" ref='Books' onClick={this.logCategory}>
-            <button type='button' className="btn btn-default" onClick={this.props.getCategoryRef}>
+          <div className="btn-group" role="group" ref='Books' onClick={this.setCategory}>
+            <button type='button' className="btn btn-default">
               <div>Books
               <img src="../assets/books.png" width="50" height="50" alt='Books' />
               </div>
             </button>
           </div>
-          <div className="btn-group" role="group">
-            <button type='button' className="btn btn-default" ref='Clothing' onClick={this.props.getCategoryRef} onClick={this.logCategory}>
+          <div className="btn-group" role="group" ref='Clothing' onClick={this.setCategory}>
+            <button type='button' className="btn btn-default">
               <div>Clothing</div>
-              <img src="../assets/clothing.png" width="50" height="50" />
+              <img src="../assets/clothing.png" width="50" height="50" alt='Clothing' />
             </button>
           </div>
-          <div className="btn-group" role="group">
-            <button type='button' className="btn btn-default" ref='Electronics' onClick={this.props.getCategoryRef} onClick={this.logCategory}>
+          <div className="btn-group" role="group" ref='Electronics' onClick={this.setCategory}>
+            <button type='button' className="btn btn-default">
               <div>Electronics</div>
-              <img src="../assets/electronics.png" width="50" height="50" />
+              <img src="../assets/electronics.png" width="50" height="50" alt='Electronics' />
             </button>
           </div>
-          <div className="btn-group" role="group">
+          <div className="btn-group" role="group" ref='Handmade' onClick={this.setCategory}>
             <button type='button' className="btn btn-default">
               <div>Handmade</div>
-              <img src="../assets/handmade.png" width="50" height="50" />
+              <img src="../assets/handmade.png" width="50" height="50" alt='Handmade' />
             </button>
           </div>
-          <div className="btn-group" role="group">
+          <div className="btn-group" role="group" ref='Health' onClick={this.setCategory}>
             <button type='button' className="btn btn-default">
               <div>Health</div>
-              <img src="../assets/health.png" width="50" height="50" />
+              <img src="../assets/health.png" width="50" height="50" alt='Health' />
             </button>
           </div>
         </div>
         <div className="btn-group btn-group-justified" aria-label="...">
-          <div className="btn-group" role="group">
+          <div className="btn-group" role="group" ref='Home' onClick={this.setCategory}>
             <button type='button' className="btn btn-default">
               <div>Home</div>
-              <img src="../assets/home.png" width="50" height="50" />
+              <img src="../assets/home.png" width="50" height="50" alt='Home' />
             </button>
           </div>
-          <div className="btn-group" role="group">
+          <div className="btn-group" role="group" ref='Money' onClick={this.setCategory}>
             <button type='button' className="btn btn-default">
               <div>Money</div>
-              <img src="../assets/money.png" width="50" height="50" />
+              <img src="../assets/money.png" width="50" height="50" alt='Money' />
             </button>
           </div>
-          <div className="btn-group" role="group">
+          <div className="btn-group" role="group" ref='Outdoor' onClick={this.setCategory}>
             <button type='button' className="btn btn-default">
               <div>Outdoor</div>
-              <img src="../assets/outdoor.png" width="50" height="50" />
+              <img src="../assets/outdoor.png" width="50" height="50" alt='Outdoor' />
             </button>
           </div>
-          <div className="btn-group" role="group">
+          <div className="btn-group" role="group" ref='Toys' onClick={this.setCategory}>
             <button type='button' className="btn btn-default">
               <div>Toys</div>
-              <img src="../assets/toys.png" width="50" height="50" />
+              <img src="../assets/toys.png" width="50" height="50" alt='Toys' />
             </button>
           </div>
-          <div className="btn-group" role="group">
+          <div className="btn-group" role="group" ref='VideosGames' onClick={this.setCategory}>
             <button type='button' className="btn btn-default">
-              <div>Movies / Video Games</div>
-              <img src="../assets/videosgames.png" width="50" height="50" />
+              <div>Movies/Games</div>
+              <img src="../assets/videosgames.png" width="50" height="50" alt='Movies/Games' />
             </button>
           </div>
         </div>
