d9c818fb19ff8ad32ddfa5e67136291c47098076
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-14 19:58:02 -0800

    [style] implements different images and adds titles

--- client/components/ItemForm.js
+++ client/components/ItemForm.js
@@ -81,44 +81,64 @@ class ItemForm extends React.Component {
         <div className='btn-group-vertical' role='group' aria-label='...'>
           <div className="btn-group btn-group-justified" aria-label="...">
             <div className="btn-group" role="group">
-              <button className="btn btn-default">
+              <button type='button' className="btn btn-default">
+                <div>Books</div>
                 <img src="../assets/books.png" width="50" height="50" />
               </button>
             </div>
             <div className="btn-group" role="group">
-              <button className="btn btn-default">
+              <button type='button' className="btn btn-default">
+                <div>Clothing</div>
                 <img src="../assets/clothing.png" width="50" height="50" />
               </button>
             </div>
             <div className="btn-group" role="group">
-              <button className="btn btn-default">
+              <button type='button' className="btn btn-default">
+                <div>Electronics</div>
                 <img src="../assets/electronics.png" width="50" height="50" />
               </button>
             </div>
             <div className="btn-group" role="group">
-              <button className="btn btn-default">
+              <button type='button' className="btn btn-default">
+                <div>Handmade</div>
                 <img src="../assets/handmade.png" width="50" height="50" />
+              </button>
+            </div>
+            <div className="btn-group" role="group">
+              <button type='button' className="btn btn-default">
+                <div>Health</div>
+                <img src="../assets/health.png" width="50" height="50" />
               </button>
             </div>
           </div>
           <div className="btn-group btn-group-justified" aria-label="...">
             <div className="btn-group" role="group">
-              <button className="btn btn-default">
-                <img src="../assets/health.png" width="50" height="50" />
-              </button>
-            </div>
-            <div className="btn-group" role="group">
-              <button className="btn btn-default">
+              <button type='button' className="btn btn-default">
+                <div>Home</div>
                 <img src="../assets/home.png" width="50" height="50" />
               </button>
             </div>
             <div className="btn-group" role="group">
-              <button className="btn btn-default">
+              <button type='button' className="btn btn-default">
+                <div>Money</div>
+                <img src="../assets/money.png" width="50" height="50" />
+              </button>
+            </div>
+            <div className="btn-group" role="group">
+              <button type='button' className="btn btn-default">
+                <div>Outdoor</div>
                 <img src="../assets/outdoor.png" width="50" height="50" />
               </button>
             </div>
             <div className="btn-group" role="group">
-              <button className="btn btn-default">
+              <button type='button' className="btn btn-default">
+                <div>Toys</div>
+                <img src="../assets/toys.png" width="50" height="50" />
+              </button>
+            </div>
+            <div className="btn-group" role="group">
+              <button type='button' className="btn btn-default">
+                <div>Movies / Video Games</div>
                 <img src="../assets/videosgames.png" width="50" height="50" />
               </button>
             </div>
