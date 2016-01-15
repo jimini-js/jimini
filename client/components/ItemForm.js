f21b33dfe32d95eb07787f5886957436ba1044fa
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-15 08:48:23 -0800

    [feat] adds categories component and logs initial category

--- client/components/ItemForm.js
+++ client/components/ItemForm.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Categories from './Categories.js'
 import $ from 'jquery';
 
 class ItemForm extends React.Component {
@@ -24,7 +25,6 @@ class ItemForm extends React.Component {
 
   getCategoryRef(ref){
     this.categoryRef = ref;
-    console.log(ref);
   }
 
   getMessageRef(ref){
@@ -73,78 +73,13 @@ class ItemForm extends React.Component {
   render(){
     return (
       <form ref='form'>
-          <label>Item</label>
+        <label>Item</label>
           <input type='text' name='itemname' className='form-control' placeholder='Item Name' ref={this.getItemnameRef} />
         <div className='form-group'>
           <label>Category</label>
           <input type='text' name='category' className='form-control' placeholder='Category' ref={this.getCategoryRef} />
         </div>
-        <div className='btn-group-vertical' role='group' aria-label='...'>
-          <div className="btn-group btn-group-justified" aria-label="...">
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default" data-category='Books' onClick={this.getCategoryRef}>
-                <div>Books</div>
-                <img src="../assets/books.png" width="50" height="50" />
-              </button>
-            </div>
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default">
-                <div>Clothing</div>
-                <img src="../assets/clothing.png" width="50" height="50" />
-              </button>
-            </div>
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default">
-                <div>Electronics</div>
-                <img src="../assets/electronics.png" width="50" height="50" />
-              </button>
-            </div>
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default">
-                <div>Handmade</div>
-                <img src="../assets/handmade.png" width="50" height="50" />
-              </button>
-            </div>
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default">
-                <div>Health</div>
-                <img src="../assets/health.png" width="50" height="50" />
-              </button>
-            </div>
-          </div>
-          <div className="btn-group btn-group-justified" aria-label="...">
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default">
-                <div>Home</div>
-                <img src="../assets/home.png" width="50" height="50" />
-              </button>
-            </div>
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default">
-                <div>Money</div>
-                <img src="../assets/money.png" width="50" height="50" />
-              </button>
-            </div>
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default">
-                <div>Outdoor</div>
-                <img src="../assets/outdoor.png" width="50" height="50" />
-              </button>
-            </div>
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default">
-                <div>Toys</div>
-                <img src="../assets/toys.png" width="50" height="50" />
-              </button>
-            </div>
-            <div className="btn-group" role="group">
-              <button type='button' className="btn btn-default">
-                <div>Movies / Video Games</div>
-                <img src="../assets/videosgames.png" width="50" height="50" />
-              </button>
-            </div>
-          </div>
-        </div>
+        <Categories getCategoryRef={this.getCategoryRef} />
         <div className='form-group'>
           <label>Description</label>
           <input type='text' name='message' className='form-control' placeholder='Description' ref={this.getMessageRef} />
