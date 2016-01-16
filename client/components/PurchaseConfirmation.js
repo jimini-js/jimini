5284c70bf2eec3e371455ca81addfb89ea613dc9
Carl Bernardo <carlbernrdo@gmail.com>
2016-01-16 12:14:05 -0800

    [feat] implements modal for purchase confirmation

--- client/components/PurchaseConfirmation.js
+++ client/components/PurchaseConfirmation.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import $ from 'jquery';
+import { Modal, Button } from 'react-bootstrap';
 
 class PurchaseConfirmation extends React.Component {
   constructor(){
     super();
-
   }
 
   render(){
     return (
-      <div>
-        Hello from Purchase Confirmation
-      </div>
+      <Modal show={this.props.showModal} onHide={this.props.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <h4>Text in a modal</h4>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.props.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
     )
   }
 }
