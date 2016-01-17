544454a5e5ea7cadf177f7f88aa7dd8fade13419
Charles Meredith <charles3m@gmail.com>
2016-01-16 16:28:41 -0800

    [style] adds github and linkedin logos and links to the footer for the project and each contributor

--- client/components/Footer.js
+++ client/components/Footer.js
@@ -9,10 +9,19 @@ class Footer extends React.Component {
         <div className="col-md-12">
           <div className="footer-container">
             <span className="footer-elements">© Jimini 2016&nbsp;</span>
+              <a href="https://github.com/jimini-js/jimini" target="_blank"><i className="fa fa-github"></i></a>
             <span className="footer-elements">Daisy Tsao&nbsp;</span>
+              <a href="https://github.com/madcurie" target="_blank"><i className="fa fa-github"></i></a>&nbsp;
+              <a href="https://www.linkedin.com/in/daisytsao" target="_blank"><i className="fa fa-linkedin"></i></a>
             <span className="footer-elements">Carl Bernardo&nbsp;</span>
+              <a href="https://github.com/carlbernardo" target="_blank"><i className="fa fa-github"></i></a>&nbsp;
+              <a href="https://www.linkedin.com/in/" target="_blank"><i className="fa fa-linkedin"></i></a>
             <span className="footer-elements">Shin Adachi&nbsp;</span>
+              <a href="https://github.com/shin064" target="_blank"><i className="fa fa-github"></i></a>&nbsp;
+              <a href="https://www.linkedin.com/in/" target="_blank"><i className="fa fa-linkedin"></i></a>
             <span className="footer-elements">Charles Meredith&nbsp;</span>
+              <a href="https://github.com/chas3m" target="_blank"><i className="fa fa-github"></i></a>&nbsp;
+              <a href="https://www.linkedin.com/in/charlesmeredith1" target="_blank"><i className="fa fa-linkedin"></i></a>
           </div>
         </div>
       </footer>
---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  nounProjectKey: "40afe5bae43247e18fa364c8fe7aa9b8",
+  nounProjectSecret: "17a114e8e7ec43eb8c244239bdd5ca6d"
+}
